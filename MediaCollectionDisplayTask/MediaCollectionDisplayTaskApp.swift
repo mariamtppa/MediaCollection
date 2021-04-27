76f4e2de0c7dceb82556507b8955b778ccd6c62a
Komeomodo <komeomdo@gmail.com>
2021-04-27 19:11:17 +0100

    Initial Commit

---
+++ MediaCollectionDisplayTask/MediaCollectionDisplayTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaCollectionDisplayTaskApp.swift
+//  MediaCollectionDisplayTask
+//
+//  Created by apple on 27/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MediaCollectionDisplayTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
